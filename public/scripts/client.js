5231ed9a174304825b1ba900fae0ae94576b927a
Grace Wang <wangxiaotian2012@gmail.com>
2022-03-30 23:58:18 +0000

    Implemented and tested logic to calculate the time diff since tweet creation and replaced the text shown on the website.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -15,7 +15,7 @@ $(() => {
     "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-    "created_at": 1648485725887
+    "created_at": 1648684074309
   };
   const user2 = {
     "user": {
@@ -38,8 +38,14 @@ $(() => {
     const $userName = $("<span>").text(tweet.user.name);
     const $divAvaName = $("<div>").addClass("user-avatar-and-name").append($avatar, $userName);
     const $divHandle = $("<div>").addClass("handle").append(`<span>${tweet.user.handle}</span>`)
+    // Calculate the day & hour difference from tweet creation tumestamp to provide the text to footer
+    const day = Math.floor((Date.now() - tweet.created_at) / (24 * 3600 * 1000));
+    const hour = Math.floor(((Date.now() - tweet.created_at) % (24 * 3600 * 1000)) / (3600 * 1000));
+    const dayText = `${day ? day : ""}${day ? (day !== 1 ? " days " : " day "): ""}`;
+    const hourText = `${hour}${hour !== 1 ? " hours " : " hour "}`;
+    const createdTimeText = `${dayText? dayText : ""}${hourText}ago`
     // Create the children elements for footer
-    const $divCreatedAt = $("<div>").addClass("created-at").text(`Created at ${Date(tweet.created_at)}`)
+    const $divCreatedAt = $("<div>").addClass("created-at").text(createdTimeText);
     const $divActionIcons = $("<div>").addClass("action-icons").append(`<i class="fa-solid fa-flag"></i>`, `<i class="fa-solid fa-retweet"></i>`, `<i class="fa-solid fa-heart"></i>`);
     // Create header, p and footer elements and append the children elements & text to them accordingly
     const $header = $("<header>").append($divAvaName, $divHandle);
