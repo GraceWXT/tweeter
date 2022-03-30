3cb718ec238939710696298e87c1ff9314e8a01d
Grace Wang <wangxiaotian2012@gmail.com>
2022-03-30 04:38:05 +0000

    Implemented JS function to adjust counter color acoording to the value. Updated the CSS file accordingly and added comments.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
-  // --- our code goes here ---
+
+  $("#tweet-text").on("input", function() {
+    // Get the input text length
+    let textLength = $("#tweet-text").val().length;
+    // Set the counter value to (140 - text length)
+    $("output.counter").val(`${140 - textLength}`);
+    // Add - delete a class "warning" when the counter is less than - not less than 0, to trigger the CSS style rule
+    let counterValue = $("output.counter").val();
+    if (counterValue < 0) {
+      $("output.counter").attr("class", "counter warning");
+    } else {
+      $("output.counter").attr("class", "counter");
+    }
+  });
+
 });
+
